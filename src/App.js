d7ea7d514e89ebdc363d0fbe880f0d4af57287d7
nallaperumal007 <nallaperumal342@gmail.com>
2024-08-08 19:46:31 +0530

    5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import CreateForm from './CreateForm';
-import Table3 from './Table3';
+
 
 const App = () => {
   return (
     <div className="App">
       <CreateForm />
-      <Table3 />
-
+    
     </div>
   );
 };
